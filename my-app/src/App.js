babbaf29a2b3ff47a5bde85046cbdb44bb0f3eda
Prithvi Kannan <prithvi.kannan@gmail.com>
2019-10-01 22:58:21 -0700

    celera logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './views/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './resources/celeraLogo.jpg' 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={logo} alt="Logo" />;
         <Home />
       </div>
     )
