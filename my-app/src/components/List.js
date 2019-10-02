37d92635f034210e2b2cddfad8eae6f53fe2fcfa
Prithvi Kannan <prithvi.kannan@gmail.com>
2019-10-02 13:52:54 -0700

    created list component

---
+++ my-app/src/components/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Row, Col, Container } from 'react-bootstrap'
+
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+            {this.props.data.map(function (d) {
+                return (<li align="left">{d}</li>)
+            })}
+            </div>
+        )
+    }
+}
--- my-app/src/views/Home.js
+++ my-app/src/views/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Row, Col, Container } from 'react-bootstrap'
 import DA6102 from '../resources/da6102.jpg'
+import List from '../components/List'
 
 export default class Home extends Component {
+
   state = {
-    name: "DA6102",
+    chip_name: "DA6102",
+    chip_image: "da6102.jpg",
     short_description: "Highly integrated, compact PMIC",
     long_description: "The DA6102 is a highly integrated multi-channel PMIC designed for 2-cell lithium-ion battery powered systems. The wide input voltage range allows direct battery connection for each channel to maximize battery life. The PMIC also includes a highly accurate input current sense with circuit breaker control for battery current sensing and over-current shutdown. The DA6102 integrates two buck regulators, a BuckBoost regulator, a high current buck controller, and two always-on LDOs. This high level of integration combined with high frequency operation (up to 3 MHz) minimizes both PCB size and external component count for the smallest possible solution size. The DA6102 uses extremely low RDSon FETs to enable very high efficiency buck and BuckBoost regulation.",
     features: ["4.5 V to 11.5 V input range",
@@ -21,11 +24,15 @@ export default class Home extends Component {
       "Handheld point-of-sale terminals",
       "2-cell battery applications"]
   }
-
+  constructor(props) {
+    super(props)
+    this.state.chip_image = "../resources/" + this.state.chip_image
+    console.log(this.state.chip_image)
+  }
   render() {
     return (
       <Container>
-        <h1> {this.state.name} </h1>
+        <h1> {this.state.chip_name} </h1>
         <Row>
           <Col>
             <h2> {this.state.short_description} </h2>
@@ -38,21 +45,15 @@ export default class Home extends Component {
         <Row>
           <Col>
             <h2> Features </h2>
-            {this.state.features.map(function (d) {
-              return (<li align="left">{d}</li>)
-            })}
+            <List data={this.state.features}/>
           </Col>
           <Col>
             <h2> Benefits </h2>
-            {this.state.benefits.map(function (d) {
-              return (<li align="left">{d}</li>)
-            })}
+            <List data={this.state.benefits}/>
           </Col>
           <Col>
             <h2> Applications </h2>
-            {this.state.applications.map(function (d) {
-              return (<li align="left">{d}</li>)
-            })}
+            <List data={this.state.applications}/>
           </Col>
         </Row>
       </Container>
