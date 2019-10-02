a7a79df7e111aa9fcb4ea70148611c331cf4af6f
Prithvi Kannan <prithvi.kannan@gmail.com>
2019-10-01 22:57:19 -0700

    added title, features, benefits, applications

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './views/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
