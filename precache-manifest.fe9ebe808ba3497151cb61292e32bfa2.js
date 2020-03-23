360dcc57652c193e7e1ee3f8816480b7e2f337f1
Prithvi Kannan <prithvi.kannan@gmail.com>
2020-03-23 13:57:29 -0700

    Updates

---
+++ precache-manifest.fe9ebe808ba3497151cb61292e32bfa2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1172c4a85ef6571ac91be8442aca88",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "ea874307688831a398bc",
+    "url": "/myapp/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "26aad342a656adb47ab0",
+    "url": "/myapp/static/css/main.b8e63fa2.chunk.css"
+  },
+  {
+    "revision": "ea874307688831a398bc",
+    "url": "/myapp/static/js/2.47c6b0a9.chunk.js"
+  },
+  {
+    "revision": "26aad342a656adb47ab0",
+    "url": "/myapp/static/js/main.c712308c.chunk.js"
+  },
+  {
+    "revision": "531475fc43af4caa365b",
+    "url": "/myapp/static/js/runtime-main.76726000.js"
+  },
+  {
+    "revision": "5c83d0f3e028dab9e122720319aa543d",
+    "url": "/myapp/static/media/XC9281-XC9282.5c83d0f3.pdf"
+  },
+  {
+    "revision": "791d60ce2414ed615c0c25a8ddbc3e5b",
+    "url": "/myapp/static/media/celeraLogo.791d60ce.jpg"
+  }
+]);
