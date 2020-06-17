988c69758e9ddb4e072cb6ecc4eb3fd4a630fe62
Prithvi Kannan <prithvi.kannan@gmail.com>
2020-06-17 12:20:45 -0700

    removed hardcode import

--- my-app/src/components/DownloadButton.js
+++ my-app/src/components/DownloadButton.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import pdf from '../resources/XC9281-XC9282.pdf'
 
 export default class DownloadButton extends Component {
     render() {
-        console.log(`./${this.props.chip}/${this.props.file}.pdf`)
         return (<a href={`./${this.props.chip}/${this.props.file}.pdf`}>Download pdf</a>)
     }
     // https://medium.com/yellowcode/download-api-files-with-react-fetch-393e4dae0d9e
